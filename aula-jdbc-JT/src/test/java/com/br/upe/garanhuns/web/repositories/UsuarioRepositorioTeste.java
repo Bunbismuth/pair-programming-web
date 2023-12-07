bc4399bf2d7b4c91fea5e6f173ab7a0d06064241
Thiago Barros <bunbismuth@proton.me>
2023-12-07 12:59:32 -0300

    feat: create UsuarioRepositorioTeste

---
+++ aula-jdbc-JT/src/test/java/com/br/upe/garanhuns/web/repositories/UsuarioRepositorioTeste.java
@@ -0,0 +1,33 @@
+package com.br.upe.garanhuns.web.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import com.br.upe.garanhuns.web.model.Usuario;
+
+@SpringBootTest
+@TestPropertySource("classpath:application-teste.properties")
+public class UsuarioRepositorioTeste {
+
+	@Autowired
+	private UsuarioRepositorio usuarioRepo;
+
+	@Test
+	public void quandoSalvaClienteEntaoPersisistido() {
+		Usuario usuario = new Usuario(423L, "02974201938", "Marcio da Carroçinha", "marcos@carroca.com");
+		this.usuarioRepo.save(usuario);
+		Usuario usuarioSalvo = this.usuarioRepo.findById(423L).get();
+		assertNotNull(usuarioSalvo, "Deveria ter salvo o cliente");
+
+	}
+
+	@BeforeEach
+	public void limparBase() {
+		this.usuarioRepo.deleteAll();
+	}
+}
